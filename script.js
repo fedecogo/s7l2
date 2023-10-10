9acc826efa0ce4e795a28c18b04e09bf4beba2fa
fedecogo <141748020+fedecogo@users.noreply.github.com>
2023-10-10 16:17:39 +0200

    versione finale

--- script.js
+++ script.js
@@ -16,3 +16,27 @@
 
     sendBtn.addEventListener('click', save);
     deleteBtn.addEventListener('click', deleteData);
+
+
+    
+    // set interval
+    let counter = 0;
+    const counterDisplay = document.getElementById('counterDisplay');
+
+    // Funzione per incrementare il contatore 
+    function incrementCounter() {
+      counter++;
+      counterDisplay.textContent = counter;
+      sessionStorage.setItem('counter', counter);
+    }
+
+    // Aggiorna il contatore ogni secondo
+    const intervalId = setInterval(incrementCounter, 1000);
+
+    // Pulisce l'intervallo quando la pagina viene chiusa
+    window.addEventListener('beforeunload', () => {
+      clearInterval(intervalId);
+    });
+
+
+    
